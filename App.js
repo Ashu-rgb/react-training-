743e5e54a823ce82b5e024f5adfd5941430a5b92
Ashutosh jha <84311243+Ashu-rgb@users.noreply.github.com>
2021-06-30 12:40:42 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,14 +4,60 @@ import './App.css';
 //import Event from './Event';
 //import Profile from'./Profile'
 //import Life from './Life'
-import Forms from './Forms';
+//import Forms from './Forms';
 
-function App() {
+class App extends React.Component{
+  constructor()
+  {
+    super();
+    this.state={
+      name:"",
+      password:"",
+      nameError:"",
+      passwordError:""
+    }
+  }
+  valid()
+  {
+    if(!this.state.name.includes("@")&& this.state.password.length<5){
+      this.setState(
+        {nameError:"Invalid Name",passwordError:"Password lenght should be more than 5"})
+    }
+    else if(!this.state.name.includes("@")){
+      this.setState(
+        {nameError:"Invalid Name"})
+    }
+    else if(this.state.password.length<5){
+      this.setState(
+        {passwordError:"Password lenght should be more than 5"})
+    }
+    else 
+    return true 
+
+  }
+  submit()
+  {
+    this.setState(
+      {nameError:"",passwordError:""})
+    if(this.valid())
+    {
+      alert("form has been submited")
+    }
+    
+  }
+  render(){
   return (
-    <div>
-    <Forms />
+    <div className="App">
+      <h1>Form Validation</h1>
+      <input type="text" onChange={(event)=>{this.setState({name:event.target.value})}} />
+      <p style={{color:"red",fontSize:"12px"}}>{this.state.nameError}</p>
+      <input type="password" onChange={(event)=>{this.setState({password:event.target.value})}} />
+      <p style={{color:"red",fontSize:"12px"}}>{this.state.passwordError}</p>
+      <button onClick={()=>this.submit()}>Submit</button>
+    
     </div>
   );
+}
 }
 
 export default App;
